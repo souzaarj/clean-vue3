dfa41151449b8c6b6e41190d5a3701003450aa3c
Nilson Souza <souza_arj@hotmail.com>
2021-04-05 18:56:27 -0300

    feat: enssure RemoteAuthentication returns an Account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -2,10 +2,13 @@ import { AccountModel } from '@/domain/models/account-model'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { HttpStatusCode } from '@/data/protocols/http-response'
 import { InvalidCredentialError } from '@/domain/errors/invalid-credential-error'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import {
+  Authentication,
+  AuthenticationParams,
+} from '@/domain/usecases/authentication'
 import { HttpPostClient } from '@/data/protocols/http-post-client'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
     private readonly httpClientPost: HttpPostClient<
@@ -13,7 +16,7 @@ export class RemoteAuthentication {
       AccountModel
     >
   ) {}
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpClientPost.post({
       url: this.url,
       body: params,
@@ -21,7 +24,7 @@ export class RemoteAuthentication {
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialError()
       default:
--- tests/domain/mocks/mock-authentication.ts
+++
@@ -1,7 +0,0 @@
-import { AuthenticationParams } from '@/domain/usecases/authentication'
-import faker from 'faker'
-
-export const mockAuthentication = (): AuthenticationParams => ({
-  password: faker.internet.password(),
-  email: faker.internet.email(),
-})
