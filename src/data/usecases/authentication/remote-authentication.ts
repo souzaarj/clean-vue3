79d1b9eee9f4c92f310c62896364ca63ed17808b
Nilson Souza <souza_arj@hotmail.com>
2021-04-05 18:16:59 -0300

    feat: ensure RemoteAuthentication throw Unexpected error on default cases

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,5 @@
+import { UnexpectedError } from './../../../domain/errors/unexpected-error'
 import { HttpStatusCode } from './../../protocols/http-response'
-import { HttpResponse } from '@/data/protocols/http-response'
 import { InvalidCredentialError } from '@/domain/errors/invalid-credential-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient } from '@/data/protocols/http-post-client'
@@ -15,8 +15,13 @@ export class RemoteAuthentication {
       body: params,
     })
 
-    if (httpResponse.statusCode === HttpStatusCode.unauthorized) {
-      throw new InvalidCredentialError()
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialError()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
