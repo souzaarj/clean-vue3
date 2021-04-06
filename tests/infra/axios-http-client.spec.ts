ade82ccfa4d7e333afbcb87101342cd26cf45000
Nilson Souza <souza_arj@hotmail.com>
2021-04-06 18:24:37 -0300

    refactor: move Sut criation to a facoty method

--- tests/infra/axios-http-client.spec.ts
+++ tests/infra/axios-http-client.spec.ts
@@ -5,10 +5,13 @@ import faker from 'faker'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient()
+}
 describe('AxiosHttpClient', () => {
   test('should call axios with correct URL', async () => {
     const url = faker.internet.url()
-    const sut = new AxiosHttpClient()
+    const sut = makeSut()
     sut.post({ url: url })
     expect(mockedAxios).toHaveBeenCalledWith(url)
   })
