09bab2f99d0bbae23a8d6e88063cd49a36479b35
Nilson Souza <souza_arj@hotmail.com>
2021-04-08 15:29:47 -0300

    refactor: add index file to organize  components

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,7 @@
+import Logo from './logo.vue'
+import LoginHeader from './login-header.vue'
+import Input from './input.vue'
+import Spinner from './spinner.vue'
+import Footer from './footer.vue'
+
+export { Logo, LoginHeader, Input, Spinner, Footer }
