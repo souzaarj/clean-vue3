0be90cddca517e6293af3f56ae8cfa02e53fbfd2
Nilson Souza <souza_arj@hotmail.com>
2021-04-08 15:27:18 -0300

    refactor: move loginHeader to a separate component

---
+++ src/presentation/components/login-header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="header">
+    <Logo class="header__image" />
+    <h1 class="header__title">{{ title }}</h1>
+  </header>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { Logo } from '@/presentation/components'
+
+  export default defineComponent({
+    name: 'LoginHeader',
+    components: {
+      Logo,
+    },
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  })
+</script>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    flex-direction: column;
+    border-top: 40px solid $primaryDark;
+    background-color: $primary;
+    align-items: center;
+
+    &__image {
+      margin-top: 40px;
+    }
+
+    &__title {
+      color: $white;
+      margin: 16px;
+    }
+  }
+</style>
--- src/presentation/components/logo.vue
+++ src/presentation/components/logo.vue
@@ -11,5 +11,3 @@
     name: 'Logo',
   })
 </script>
-
-<style lang="scss" scoped></style>
