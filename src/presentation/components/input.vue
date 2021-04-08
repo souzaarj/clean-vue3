eb398fcc22d50e72b98be1a591d943aa5074d8af
Nilson Souza <souza_arj@hotmail.com>
2021-04-08 15:26:34 -0300

    refactor: move input to a separate component

---
+++ src/presentation/components/input.vue
@@ -0,0 +1,31 @@
+<template>
+  <input class="input" type="{type}" placeholder="{placeHolder}" />
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Input',
+    props: {
+      type: {
+        type: String,
+        default: 'text',
+        required: true,
+      },
+      placeHolder: {
+        type: String,
+        default: '',
+        required: false,
+      },
+    },
+  })
+</script>
+
+<style lang="scss">
+  .input {
+    &:focus {
+      outline-color: $primaryLight;
+    }
+  }
+</style>
