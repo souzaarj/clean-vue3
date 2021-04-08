a7b0467455789ab195a939c6806c401c46e324c0
Nilson Souza <souza_arj@hotmail.com>
2021-04-08 15:25:11 -0300

    refactor: move footer to a separate component

---
+++ src/presentation/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Footer',
+  })
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    background-color: $primary;
+    height: 48px;
+  }
+</style>
