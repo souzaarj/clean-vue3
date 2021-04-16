67454595b4fdf56c97f746ec8a09c00e418bff32
Nilson Souza <souza_arj@hotmail.com>
2021-04-16 15:00:06 -0300

    refactor: change jest config

---
+++ jest.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from '@jest/types'
+
+const esModules = ['@vue/test-utils'].join('|')
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts',
+  ],
+
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'vue', 'node'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.vue$': 'vue-jest',
+  },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+  moduleNameMapper: {
+    '@/tests/(.*)$': '<rootDir>/tests/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  testMatch: [
+    '**/tests/**/*.(test|spec).(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+  ],
+}
+
+export default config
