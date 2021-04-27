3268e490620e9dc79bb0d61629568e352b2658dc
Nilson Souza <souza_arj@hotmail.com>
2021-04-27 16:36:47 -0300

    feat: return falsy if field is not empty

--- __tests__/presentation/validation/required-field-validation.spec.ts
+++ __tests__/presentation/validation/required-field-validation.spec.ts
@@ -3,9 +3,15 @@ import { RequiredFieldError } from '@/validation/errors'
 import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
-  test('should return error if filed is empty', () => {
+  test('should return error if field is empty', () => {
     const sut = new RequiredFieldValidation(faker.random.word())
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation(faker.random.word())
+    const result = sut.validate(faker.random.word())
+    expect(result).toBeFalsy()
   })
 })
