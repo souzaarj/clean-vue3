43ce47f221de70ddcd6bfbb006def263945ccbc3
Nilson Souza <souza_arj@hotmail.com>
2021-04-30 20:28:45 -0300

    test: ensure ValidationComposite retuns the first error found

--- __tests__/validation/validators/validation-composite.spec.ts
+++ __tests__/validation/validators/validation-composite.spec.ts
@@ -5,13 +5,16 @@ import faker from 'faker'
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
     const field = faker.database.column()
-    const errorMessage = faker.random.word()
+    const firstErrorMessage = faker.random.word()
+    const secondErrorMessage = faker.random.word()
     const validationSpy = new ValidationSpy(field)
+    validationSpy.error = new Error(firstErrorMessage)
     const validationSpy2 = new ValidationSpy(field)
-    validationSpy2.error = new Error(errorMessage)
+    validationSpy2.error = new Error(secondErrorMessage)
 
     const sut = new ValidationComposite([validationSpy, validationSpy2])
     const error = sut.validate(field, faker.random.word())
     expect(error).toBe(error)
+    expect(error).toBe(firstErrorMessage)
   })
 })
