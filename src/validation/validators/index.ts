a3a70789d07ccd08955826bf56c3be37ba028294
Nilson Souza <souza_arj@hotmail.com>
2021-05-01 12:32:58 -0300

    feat: ensure ValidationBuilder has a required helper

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,3 +1,5 @@
 export * from './email-validation'
 export * from './required-field-validation'
 export * from './min-length-validation'
+export * from './validation-composite'
+export * from './validation-composite'
