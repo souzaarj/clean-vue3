60172d74900759d90fa9c8feae92e8f417efaac8
Nilson Souza <souza_arj@hotmail.com>
2021-05-08 20:11:46 -0300

    test: ensure CompareFieldsValidation return error if compare is invalid

---
+++ __tests__/validation/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,14 @@
+import { CompareFieldsValidation } from '@/validation/validators/compare-fields-validation'
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
+import faker from 'faker'
+
+const makeSut = (valueToCompare: string): CompareFieldsValidation =>
+  new CompareFieldsValidation(faker.database.column(), valueToCompare)
+
+describe('CompareFieldsValidation', () => {
+  test('should return error if compare is invalid', () => {
+    const sut = makeSut(faker.random.word())
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
