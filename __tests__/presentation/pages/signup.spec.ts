f554b9debac84173374155745c8c36531b6b6da1
Nilson Souza <souza_arj@hotmail.com>
2021-05-11 18:51:06 -0300

    test: ensure Signup show name error if validation fails

--- __tests__/presentation/pages/signup.spec.ts
+++ __tests__/presentation/pages/signup.spec.ts
@@ -1,9 +1,14 @@
 import { ValidationSpy } from '@/tests/presentation/mocks'
-import { populateField } from './../helper/test-helper'
+import {
+  populateField,
+  simulateStatusField,
+} from '@/tests/presentation/helper/test-helper'
+
 import {
   testFieldStatus,
   testFieldChildCount,
 } from '@/tests/presentation/helper/test-helper'
+
 import { Signup } from '@/presentation/pages'
 import { mount, VueWrapper } from '@vue/test-utils'
 import router from '@/presentation/router/router'
@@ -13,11 +18,14 @@ type SutTypes = {
   sut: VueWrapper<any>
   validationSpy: ValidationSpy
 }
+type SutParams = {
+  validationError: string
+}
 
-const makeSut = (): SutTypes => {
+const makeSut = (paramError?: SutParams): SutTypes => {
   const validationSpy = new ValidationSpy()
   const props = { validation: validationSpy }
-
+  validationSpy.errorMessage = paramError?.validationError || ''
   const sut = mount(Signup, {
     props,
     global: {
@@ -80,5 +88,13 @@ describe('Signup', () => {
     await populateField(sut, 'passwordConfirmation', passwordConfirmation)
     expect(validationSpy.fieldName).toBe('passwordConfirmation')
     expect(validationSpy.fieldValue).toBe(passwordConfirmation)
+  })
+
+  test('should show name error if validation fails', async () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+    const name = faker.internet.userName()
+    await populateField(sut, 'name', name)
+    simulateStatusField(sut, 'name', validationError)
   })
 })
